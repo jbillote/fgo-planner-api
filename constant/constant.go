c8ec92f236fa5684d1f745a1fc2127fb1c025a44
jbillote <jbillote@users.noreply.github.com>
2023-10-27 17:29:15 -0700

    Add servant info URL constant

--- constant/constant.go
+++ constant/constant.go
@@ -1,4 +1,5 @@
 package constant
 
-const AtlasAcademySearch = "https://api.atlasacademy.io/nice/JP/servant/search?name=%s&lang=en"
 const AtlasAcademyClassIcon = "https://static.atlasacademy.io/JP/ClassIcons/class%d_%d.png"
+const AtlasAcademySearch = "https://api.atlasacademy.io/nice/JP/servant/search?name=%s&lang=en"
+const AtlasAcademyServantInfo = "https://api.atlasacademy.io/nice/JP/servant/%d?lang=en"
