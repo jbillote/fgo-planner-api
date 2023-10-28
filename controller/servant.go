e9f268634cab2bc3896b03c294d8d27a2f9802e1
jbillote <jbillote@users.noreply.github.com>
2023-10-27 19:01:08 -0700

    Fetch character ascension materials

--- controller/servant.go
+++ controller/servant.go
@@ -9,6 +9,8 @@ import (
     "io"
     "math"
     "net/http"
+    "sort"
+    "strconv"
 )
 
 type servantResponse struct {
@@ -18,7 +20,7 @@ type servantResponse struct {
     Rarity             int                  `json:"rarity"`
     ExtraAssets        extraAssets          `json:"extraAssets"`
     Skills             []model.Skill        `json:"skills"`
-    Appends            []model.Skill        `json:"appendPassive"`
+    Appends            []appendPassive      `json:"appendPassive"`
     AscensionMaterials map[string]materials `json:"ascensionMaterials"`
     SkillMaterials     map[string]materials `json:"skillMaterials"`
     AppendMaterials    map[string]materials `json:"appendSkillMaterials"`
@@ -31,6 +33,10 @@ type extraAssets struct {
 
 type characterImages struct {
     Ascension map[string]string `json:"ascension"`
+}
+
+type appendPassive struct {
+    Skill model.Skill `json:"skill"`
 }
 
 type materials struct {
@@ -88,6 +94,92 @@ func SearchServant(c echo.Context) (err error) {
 }
 
 func GetServant(c echo.Context) (err error) {
-    id := c.Param("id")
-    return c.String(http.StatusOK, id)
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.Logger().Fatal(err)
+        return c.String(http.StatusInternalServerError, err.Error())
+    }
+    uri := fmt.Sprintf(constant.AtlasAcademyServantInfo, id)
+
+    resp, err := http.Get(uri)
+    if err != nil {
+        c.Logger().Fatal(err)
+        return c.String(http.StatusInternalServerError, err.Error())
+    }
+
+    defer resp.Body.Close()
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        c.Logger().Fatal(err)
+        return c.String(http.StatusInternalServerError, err.Error())
+    }
+
+    var s servantResponse
+    err = json.Unmarshal(body, &s)
+    if err != nil {
+        c.Logger().Fatal(err)
+        return c.String(http.StatusInternalServerError, err.Error())
+    }
+
+    portraits := make([]string, 0, len(s.ExtraAssets.CharacterGraph.Ascension))
+    keys := make([]string, 0, len(s.ExtraAssets.CharacterGraph.Ascension))
+    for k := range s.ExtraAssets.CharacterGraph.Ascension {
+        keys = append(keys, k)
+    }
+    sort.Strings(keys)
+    for _, v := range keys {
+        portraits = append(portraits, s.ExtraAssets.CharacterGraph.Ascension[v])
+    }
+
+    skills := make([]model.Skill, 0, len(s.Skills))
+    for _, v := range s.Skills {
+        skills = append(skills, model.Skill{
+            Name: v.Name,
+            Icon: v.Icon,
+        })
+    }
+
+    appends := make([]model.Skill, 0, len(s.Appends))
+    for _, v := range s.Appends {
+        appends = append(appends, model.Skill{
+            Name: v.Skill.Name,
+            Icon: v.Skill.Icon,
+        })
+    }
+
+    ascensionMaterials := make([]model.MaterialList, 0, len(s.AscensionMaterials))
+    keys = make([]string, 0, len(s.AscensionMaterials))
+    for k := range s.AscensionMaterials {
+        keys = append(keys, k)
+    }
+    sort.Strings(keys)
+    for _, v := range keys {
+        items := make([]model.Material, 0, len(s.AscensionMaterials[v].Items))
+        for _, i := range s.AscensionMaterials[v].Items {
+            items = append(items, model.Material{
+                ID:     i.Details.ID,
+                Name:   i.Details.Name,
+                Icon:   i.Details.Icon,
+                Amount: i.Amount,
+            })
+        }
+
+        ascensionMaterials = append(ascensionMaterials, model.MaterialList{
+            Materials: items,
+            QP:        s.AscensionMaterials[v].QP,
+        })
+    }
+
+    servant := model.Servant{
+        ID:        s.ID,
+        Name:      s.Name,
+        ClassIcon: fmt.Sprintf(constant.AtlasAcademyClassIcon, int(math.Min(3, float64(s.Rarity))), s.ClassID),
+        Icon:      s.ExtraAssets.Faces.Ascension["1"],
+        Portraits: portraits,
+        Skills:    skills,
+        Appends:   appends,
+    }
+
+    return c.JSON(http.StatusOK, servant)
 }
--- model/material_list.go
+++ model/material_list.go
@@ -2,5 +2,5 @@ package model
 
 type MaterialList struct {
     Materials []Material `json:"materials"`
-    Qp        int        `json:"qp"`
+    QP        int        `json:"qp"`
 }
