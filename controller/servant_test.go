51824dfb79983646240cd231a510715005b81277
jbillote <jbillote@users.noreply.github.com>
2023-10-30 15:33:29 -0700

    Test getSortedKeys

--- controller/servant_test.go
+++ controller/servant_test.go
@@ -2,26 +2,68 @@ package controller
 
 import "testing"
 
+var mapUnsortedKeys = map[string]string{
+	"o": "test",
+	"g": "test",
+	"f": "test",
+}
+
+var mapSortedKeys = map[string]string{
+	"f": "test",
+	"g": "test",
+	"o": "test",
+}
+
+func TestGetSortedKeysSortedInput(t *testing.T) {
+	got := getSortedKeys(mapSortedKeys)
+	want := []string{"f", "g", "o"}
+	if !arrayEquality(got, want) {
+		t.Errorf("Got %s, expected %s", got, want)
+	}
+}
+
+func TestGetSortedKeysUnsortedInput(t *testing.T) {
+	got := getSortedKeys(mapUnsortedKeys)
+	want := []string{"f", "g", "o"}
+	if !arrayEquality(got, want) {
+		t.Errorf("Got %s, expected %s", got, want)
+	}
+}
+
 func TestClassIconFilenameRarityInRange(t *testing.T) {
-	want := classIconFilename(2, 1)
-	got := "2_1"
+	got := classIconFilename(2, 1)
+	want := "2_1"
 	if got != want {
 		t.Errorf("Got %s, expected %s", got, want)
 	}
 }
 
 func TestClassIconFilenameRarityGreater(t *testing.T) {
-	want := classIconFilename(9999, 1)
-	got := "3_1"
+	got := classIconFilename(9999, 1)
+	want := "3_1"
 	if got != want {
 		t.Errorf("Got %s, expected %s", got, want)
 	}
 }
 
 func TestClassIconFilenameRarityNegative(t *testing.T) {
-	want := classIconFilename(-1, 1)
-	got := "1_1"
+	got := classIconFilename(-1, 1)
+	want := "1_1"
 	if got != want {
 		t.Errorf("Got %s, expected %s", got, want)
 	}
+}
+
+func arrayEquality(a []string, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
 }
